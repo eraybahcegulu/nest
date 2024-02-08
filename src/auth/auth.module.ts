6b3aaae37b5797b34f3f50023d467020d7688c05
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-08 15:33:49 +0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       imports:[ConfigModule],
       useFactory:(configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '30d'},
+        //signOptions: { expiresIn: '30d'},
       }),
       inject: [ConfigService],
     })
