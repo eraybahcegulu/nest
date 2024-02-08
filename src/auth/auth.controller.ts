6f5492e505a2ebbc28f78048c26fafc63788e350
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-08 15:34:01 +0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,19 +5,30 @@ import { JwtService } from '@nestjs/jwt';
 export class AuthController {
   constructor(private readonly jwtService: JwtService) { }
 
+  /*
+    {
+      "email": "eraybahcegulu@gmail.com",
+      "password": "eraybahcegulu"
+    }
+  */
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Req() req) {
     const { id, email } = req.user;
     const token = this.jwtService.sign({ id, email });
-    return {token};
+    return { token };
   }
 
+  /*
+    {
+      Headers.authorization => Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWMwZDg3YWMwNjA2NDE5MWFjYTMxMzkiLCJ1c2VyTmFtZSI6ImVyYXliYWhjZWd1bHUiLCJlbWFpbCI6ImVyYXliYWhjZWd1bHVAZ21haWwuY29tIiwidXNlclJvbGUiOiJhZG1pbiIsImlhdCI6MTcwNzEzOTk5Nn0.FZcVkIwoOldPNodqSpNYfeEOe1nvVibUAOYmMPafu-w
+    }
+  */
   @Get('getUserInfo')
   @UseGuards(AuthGuard('jwt'))
-  getUserInfo(@Req() req, ) {
+  getUserInfo(@Req() req,) {
     const { id, email } = req.user;
     const token = this.jwtService.sign({ id, email });
-    return {id, email, token};
+    return { id, email, token };
   }
 }
