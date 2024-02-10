5585b3087ef00eed103b83f4a1e46349a13a20eb
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-10 17:25:28 +0300

    category api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { AuthModule } from './auth/auth.module';
       }),
       inject: [ConfigService],
     }),
+    CategoryModule,
   ],
 })
 export class AppModule { }
